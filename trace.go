938369fc01878452053f4e8ce7d2652600a348cd
zhs <haishan.zhou@magewell.com>
2017-08-28 10:06:58 +0800

    udpate testcase

--- trace.go
+++ trace.go
@@ -2,13 +2,14 @@ package goutils
 
 import (
 	"fmt"
+	"io"
 	"runtime"
 )
 
-func Trace() {
+func Trace(w io.Writer) {
 	f, file, line, ok := runtime.Caller(1)
 	if !ok {
 		return
 	}
-	fmt.Printf("%s (%s:%d)\n", runtime.FuncForPC(f).Name(), file, line)
+	fmt.Fprintf(w, "%s (%s:%d)\n", runtime.FuncForPC(f).Name(), file, line)
 }
--- trace_test.go
+++ trace_test.go
@@ -1,7 +1,20 @@
 package goutils
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+)
 
 func Test_trace(t *testing.T) {
-	Trace()
+	gopath := os.Getenv("GOPATH")
+	expect := "github.com/choueric/goutils.Test_trace (" + gopath + "/src/github.com/choueric/goutils/trace_test.go:13)\n"
+	var result bytes.Buffer
+	Trace(&result)
+
+	if expect != result.String() {
+		t.Error("Trace info dose not match.")
+		t.Error(result.String())
+		t.Error(expect)
+	}
 }
