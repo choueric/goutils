15fb2681f237fdf296461debf94f2aea83615a59
choueric <zhssmail@gmail.com>
2018-04-25 15:56:13 +0800

    add io module

---
+++ io.go
@@ -0,0 +1,16 @@
+package goutils
+
+import (
+	"bufio"
+	"strings"
+)
+
+func ReadLine(r *bufio.Reader) (string, error) {
+	str, err := r.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	str = strings.Trim(str, "\r\n")
+	return str, nil
+}
